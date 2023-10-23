a1195d482bd76fec69794e350484fd4f532ec158
TandresA <76996347+TandresA@users.noreply.github.com>
2023-10-23 15:53:02 +0200

    Test

---
+++ src/main/java/api/training_for_green/TrainingForGreenApplication.java
@@ -0,0 +1,13 @@
+package api.training_for_green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingForGreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingForGreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/training_for_green/TrainingForGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package api.training_for_green;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingForGreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
